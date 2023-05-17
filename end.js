336f5c200575a1110f45f04202b3c4a541c55f3f
KrisTechHub <114846706+KrisTechHub@users.noreply.github.com>
2023-05-17 21:48:48 +0800

    save high scores in local storage

--- end.js
+++ end.js
@@ -5,13 +5,36 @@ const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem("mostRecentScore");
 finalScore.innerText = mostRecentScore;
 
+const MAX_HIGHSCORES = 5;
+const highScores = JSON.parse(localStorage.getItem("highScores")) || []; //make into an array using JSON.parse()
+
+
 username.addEventListener('keyup', (e) => {
     saveScoreBtn.disabled = !username.value;
 });
 
 saveScore = (e) => {
-    console.log('saved!');
     e.preventDefault();
 
+    const score = {
+        score: mostRecentScore,
+        name: username.value
+    };
+
+    highScores.push(score);
+    username.value = '';
+    finalScore.innerText = 0;
+
+    //SAVE ONLY TOP 5 SCORES, REMOVE LOWER SCORES
+    highScores.sort((a,b) => {
+        return b.score - a.score;
+    });
+
+    highScores.splice(5); //cut-off all lowest scores
+
+    localStorage.setItem('highScores', JSON.stringify(highScores)); //update high scores, make into string by using JSON.stringigy()
+    username.style.display = "none";
+    saveScoreBtn.style.display = "none";
+    finalScore.style.display = "none";
 };
 
