61b03a99f60d5e178271cf9e9ed7ffb1dad95a77
KrisTechHub <114846706+KrisTechHub@users.noreply.github.com>
2023-05-17 17:15:13 +0800

    setting up js

--- game.js
+++ game.js
@@ -1,1 +1,8 @@
-console.log("hahaha");
+const question = document.getElementById('question');
+const choices = Array.from(document.getElementsByClassName('choice-text'));
+
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestins = [];
