32cec49e1d1902e8610fde6b201b20283927b813
KrisTechHub <114846706+KrisTechHub@users.noreply.github.com>
2023-05-17 18:30:41 +0800

    next question functionality

--- game.js
+++ game.js
@@ -2,7 +2,7 @@ const question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName('choice-text'));
 
 let currentQuestion = {};
-let acceptingAnswers = true;
+let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
@@ -37,13 +37,48 @@ let questions = [
 
 // CONSTANTS //
 const CORRECT_BONUS = 10;
-const MAX_QUESTIONS = 3;
+const MAX_QUESTIONS = questions.length;
+
+console.log(MAX_QUESTIONS);
 
 startGame = () => {
     questionCounter = 0;
     score = 0;
     availableQuestions = [ ...questions] //spread operator
     console.log(availableQuestions);
+    getNewQuestion();
 };
+
+getNewQuestion = () => {
+
+    if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+        //go to the end page
+        return window.location.assign("/end.html");
+    }
+
+    questionCounter++;
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+
+    choices.forEach(choice => {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number]
+    });
+
+    availableQuestions.splice(questionIndex, 1);
+    acceptingAnswers = true;
+};
+
+choices.forEach(choice => {
+    choice.addEventListener('click', (e) => {
+        if (!acceptingAnswers) return;
+
+        acceptingAnswers = false;
+        const selectedChoie = e.target;
+        const selectedAnswer = selectedChoie.dataset["number"];
+        getNewQuestion();
+    });
+})
 
 startGame();
