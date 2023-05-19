e6d2c6dc4e6a25ec4843053cb1e9d223b003ac1d
KrisTechHub <114846706+KrisTechHub@users.noreply.github.com>
2023-05-19 18:47:25 +0800

    errors

--- game.js
+++ game.js
@@ -9,35 +9,20 @@ let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
+let questions = [];
 
-let questions = [
-    {
-        question: "Inside which HTML element do we put the JavaScript??",
-        choice1: "<script>",
-        choice2: "<javascript>",
-        choice3: "<js>",
-        choice4: "<scripting>",
-        answer: 1
-      },
-      {
-        question:
-          "What is the correct syntax for referring to an external script called 'xxx.js'?",
-        choice1: "<script href='xxx.js'>",
-        choice2: "<script name='xxx.js'>",
-        choice3: "<script src='xxx.js'>",
-        choice4: "<script file='xxx.js'>",
-        answer: 3
-      },
-      {
-        question: " How do you write 'Hello World' in an alert box?",
-        choice1: "msgBox('Hello World');",
-        choice2: "alertBox('Hello World');",
-        choice3: "msg('Hello World');",
-        choice4: "alert('Hello World');",
-        answer: 4
-      }
-];
 
+fetch("questions.json")
+    .then(res => {
+        return res.json();
+    })
+    .then(loadedQuestions => {
+        questions = loadedQuestions;
+        startGame();
+    })
+    .catch(err => {
+        console.error(err);
+    });
 
 // CONSTANTS //
 const CORRECT_BONUS = 10;
@@ -57,7 +42,7 @@ getNewQuestion = () => {
         localStorage.setItem("mostRecentScore", score);
         //go to the end page
         return window.location.assign("/end.html");
-    };
+    }
 
     questionCounter++;
     progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
@@ -93,6 +78,7 @@ choices.forEach(choice => {
         }
  
         selectedChoice.parentElement.classList.add(classToApply);
+
         setTimeout(() => {
             selectedChoice.parentElement.classList.remove(classToApply);
             getNewQuestion();
@@ -101,10 +87,7 @@ choices.forEach(choice => {
 });
 
 
-incrementScore = num => {
-    score +=num;
+incrementScore = (num) => {
+    score += num;
     scoreText.innerText = score;
 };
-
-
-startGame();
